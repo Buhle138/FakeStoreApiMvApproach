e285491c4a1a935f1f7b49637b1c93b99c6e46cb
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-06 11:27:13 +0200

    Initial Commit

---
+++ FakeStoreApiMvApproach/FakeStoreApiMvApproachApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApiMvApproachApp.swift
+//  FakeStoreApiMvApproach
+//
+//  Created by Buhle Radzilani on 2024/06/06.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApiMvApproachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
